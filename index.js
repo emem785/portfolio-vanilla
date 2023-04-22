008c2ef4104bcc72a6851678db3a89574c0eba6f
Emem <ememisong62@gmail.com>
2023-04-22 19:21:39 +0100

    Fix - alignment issues

--- index.js
+++ index.js
@@ -6,9 +6,3 @@ if (isIndex && !isLarge) {
 	document.getElementsByTagName("body")[0].style.margin = "1em 3em";
 }
 
-
-document.getElementById("email").addEventListener("click", openEmail);
-
-function openEmail() {
-	window.location.href = "mailto:ememisong62@gmail.com";
-}
